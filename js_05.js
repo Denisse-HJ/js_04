e3cb8cd65b11ea5978878cd9a43a944b7b3bbe24
Denisse-HJ <arq.denisseher@gmail.com>
2024-01-13 16:24:54 -0600

    js_5 eje_5factorial

--- js_05.js
+++ js_05.js
@@ -1,5 +1,5 @@
-/*  User profile */
-
+/*  1 User profile */
+/*
 
 let user = window.prompt("What's your username?");
 
@@ -14,8 +14,8 @@ for (index = 0; index < 3; index++) {
 
     console.log(`Username: ${user} Age: ${age} and the film ${movie[0]} is one of my favorites`);
 
-/* Highest number */
-
+/* 2 Highest number */
+/*
 
 let number = [];
 
@@ -26,15 +26,14 @@ for (let i = 0; i < 10; i++) {
 
 let max = number.reduce(function (a, b) {
     return Math.max(a, b);
-
 }, -Infinity);
 
     console.log(`The largest number is: ${max}`);
 
 
-/* Alarm */
+/* 3 Alarm */
 
-
+/*
 let seconds = prompt('You want to set a sleep alert, tell me in how many seconds:')
 seconds = seconds * 1000;
 //convierte a un número entero.
@@ -45,41 +44,41 @@ setTimeout(() => {
 }, convertido);
 
 
-/** Palindrome
+/** 4 Palindrome
 Write a program that prompts for a word or sentence (it can be capitalized, have spaces and punctuation). 
 Figure out if the sentence/word is a palindrome or not. Ignoring punctuation, spaces and capitalized letters.
 */
 
 
 
-/**
- * Factorial
+/** 5 Factorial
 Write a program that prompts for an intenger number n. Where  1 <= n. Solve this using recursion.
  */
 
-function number() {
-    let conv;
-
-    while (true) {
+    while (true) { //Genero un ciclo para ue hasta que se cumpla el if-else me imprima el mensaje, de lo contrario el prompt no se cerrara.
         let n = prompt("write an integer number"); 
 
-        let conv = parseInt(n, 10);
+        let conv = parseInt(n, 10); //convierto a numero entero
 
         if (1 <= conv) {
-            console.log(`The number you provided is: ${conv}`);
+            console.log(`The number you provided is: ${conv} and the factorial of this number is ${factorial(conv)}`)// aqui en la consola
             break;
         } else {
             console.log("Please enter a numerical value greater than 1.");
         }
     }
 
-    return conv;
+function factorial(conv) { // Función factorial que pone como parametro conv
+        if (conv === 1) { //si conv = 1 con el reguiente return 1; rompe, pero si da otro valor continua en return conv.
+        return 1;
+}   
+        return conv * factorial(conv - 1); //este multiplica conv * factorial, pero con -1.
 }
-number();
+
+
 
     
-/**
- * Flat array
+/** 6Flat array
 Write a program that takes the following nested matrix and flattens it (makes it a 1D array). 
 Use any type of algorithm you want like callbacks, recursion, etc...
 
@@ -87,4 +86,4 @@ Use any type of algorithm you want like callbacks, recursion, etc...
 let multiDimension = [1, [2, 3, [4, 5, [6]]]];
 
 
- */
+*/ 
